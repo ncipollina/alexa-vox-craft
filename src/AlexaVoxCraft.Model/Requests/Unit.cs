c0c6fed02369259452b250df51a98887f8fed150
Nick Cipollina <ncipollina@gmail.com>
2025-01-28 12:58:00 -0500

    Removed required from the Unit fields

--- src/AlexaVoxCraft.Model/Requests/Unit.cs
+++ src/AlexaVoxCraft.Model/Requests/Unit.cs
@@ -5,8 +5,8 @@ namespace AlexaVoxCraft.Model.Requests;
 public class Unit
 {
     [JsonPropertyName("unitId")]
-    public required string UnitId { get; set; }
+    public string? UnitId { get; set; }
 
     [JsonPropertyName("persistentUnitId")]
-    public required string PersistentUnitId { get; set; }
+    public string? PersistentUnitId { get; set; }
 }
