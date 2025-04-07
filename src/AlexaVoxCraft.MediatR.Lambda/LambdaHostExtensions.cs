428c253295dcb2c44822453df1c2424dc9b7950d
Nick Cipollina <ncipollina@gmail.com>
2025-04-07 10:51:35 -0400

    Implement custom Alexa Lambda serializer and logging extensions

---
+++ src/AlexaVoxCraft.MediatR.Lambda/LambdaHostExtensions.cs
@@ -0,0 +1,52 @@
+using AlexaVoxCraft.Model.Request;
+using AlexaVoxCraft.Model.Response;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace AlexaVoxCraft.MediatR.Lambda;
+
+public static class LambdaHostExtensions
+{
+    public static async Task<int> RunAlexaSkill<T>(
+        Func<T, IServiceProvider, Func<SkillRequest, ILambdaContext, Task<SkillResponse>>>? handlerBuilder = null,
+        Func<IServiceProvider, ILambdaSerializer>? serializerFactory = null
+    ) where T : AlexaSkillFunction, new()
+    {
+        try
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new CompactJsonFormatter())
+                .CreateBootstrapLogger();
+
+            Log.Information("🚀 Starting Lambda Host");
+
+            var function = new T();
+            var services = function.ServiceProvider;
+
+            var handler = handlerBuilder?.Invoke(function, services) ?? function.FunctionHandlerAsync;
+
+            var serializer = serializerFactory?.Invoke(services)
+                             ?? services.GetRequiredService<ILambdaSerializer>();
+
+            await LambdaBootstrapBuilder.Create(handler, serializer)
+                .Build()
+                .RunAsync();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "💥 Host terminated unexpectedly");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
