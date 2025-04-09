901aea58477d5712f4f85e46693910f4cdd75b43
Nick Cipollina <ncipollina@gmail.com>
2025-04-09 13:32:29 -0400

    Implement APL document rendering in DefaultHandler

--- samples/Sample.Apl.Function/Handlers/DefaultHandler.cs
+++ samples/Sample.Apl.Function/Handlers/DefaultHandler.cs
@@ -1,4 +1,8 @@
+using System.Text;
 using AlexaVoxCraft.MediatR;
+using AlexaVoxCraft.Model.Apl;
+using AlexaVoxCraft.Model.Apl.Commands;
+using AlexaVoxCraft.Model.Apl.Components;
 using AlexaVoxCraft.Model.Response;
 
 namespace Sample.Apl.Function.Handlers;
@@ -12,6 +16,97 @@ public class DefaultHandler : IDefaultRequestHandler
 
     public async Task<SkillResponse> Handle(IHandlerInput input, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var speechOutput = new StringBuilder("Hello world!");
+        var document = new APLDocument(APLDocumentVersion.V2023_2)
+        {
+            Layouts = new Dictionary<string, Layout>
+            {
+                ["TextFrame"] = new Layout
+                {
+                    Parameters = new List<Parameter>
+                    {
+                        "text",
+                        "color"
+                    },
+                    Items = new List<APLComponent>
+                    {
+                        new Frame
+                        {
+                            BorderColor = "${color}",
+                            BorderWidth = 5,
+                            Height = "100%",
+                            Width = "100%",
+                            Shrink = 1,
+                            Item = new List<APLComponent>
+                            {
+                                new Text
+                                {
+                                    Content = "${text}",
+                                    TextAlign = "center",
+                                    Width = "100%",
+                                    Height = "100%",
+                                    TextAlignVertical = "center"
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            MainTemplate = new Layout(new Container
+            {
+                Id = "main",
+                Width = "100%",
+                Height = "100%",
+                Padding = new List<int> { 20 },
+                Items = new List<APLComponent>
+                {
+                    new CustomComponent("TextFrame")
+                    {
+                        Id = "textFrame",
+                        Properties = new Dictionary<string, object>
+                        {
+                            ["text"] = "Hello world!",
+                            ["color"] = "yellow"
+                        }
+                    }
+                }!
+            }),
+            OnMount = new List<APLCommand>
+            {
+                new InsertItem
+                {
+                    DelayMilliseconds = 5000,
+                    ComponentId = "main",
+                    Items = new List<object>
+                    {
+                        new CustomComponent("TextFrame")
+                        {
+                            Id = "textFrame",
+                            Properties = new Dictionary<string, object>
+                            {
+                                ["text"] = "Hello world!",
+                                ["color"] = "yellow"
+                            }
+                        }
+                    },
+                }
+            }
+        };
+        var directive = new RenderDocumentDirective
+        {
+            Document = document
+        };
+        if (input.RequestEnvelope.APLSupported())
+        {
+            speechOutput.Clear();
+
+
+            input.ResponseBuilder.AddDirective(directive);
+        }
+        return await input.ResponseBuilder
+            .Speak(speechOutput.ToString())
+            .Reprompt(speechOutput.ToString())
+            .WithSimpleCard("APL Sample", speechOutput.ToString())
+            .GetResponse(cancellationToken);
     }
 }
