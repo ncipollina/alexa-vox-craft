9796d4940d749ab263e521e62901ca42e14ee6d3
Nick Cipollina <ncipollina@gmail.com>
2025-04-17 09:23:14 -0400

    Refactor serialization methods in AlexaLambdaSerializer for improved logging and code clarity

--- src/AlexaVoxCraft.MediatR.Lambda/Serialization/AlexaLambdaSerializer.cs
+++ src/AlexaVoxCraft.MediatR.Lambda/Serialization/AlexaLambdaSerializer.cs
@@ -18,45 +18,24 @@ public sealed class AlexaLambdaSerializer : ILambdaSerializer
         _options = jsonSerializerOptions ?? AlexaJsonOptions.DefaultOptions;
     }
 
-    public T Deserialize<T>(Stream requestStream)
+    public T? Deserialize<T>(Stream requestStream)
     {
-        return _logger.IsEnabled(LogLevel.Debug)
-            ? DeserializeWithLogging<T>(requestStream)
-            : JsonSerializer.Deserialize<T>(requestStream, _options)!;
-    }
+        var obj = JsonSerializer.Deserialize<T>(requestStream, _options);
+        if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            _logger.Debug("📥 Raw JSON Input: {@RawRequest}", obj);
+        }
 
-    private T DeserializeWithLogging<T>(Stream requestStream)
-    {
-        using var reader = new StreamReader(requestStream, leaveOpen: true);
-        var json = reader.ReadToEnd();
-        requestStream.Position = 0;
-
-        _logger.Debug("📥 Raw JSON Input: {Json}", json);
-
-        return JsonSerializer.Deserialize<T>(json, _options)!;
+        return obj;
     }
 
     public void Serialize<T>(T response, Stream responseStream)
     {
         if (_logger.IsEnabled(LogLevel.Debug))
         {
-            SerializeWithLogging(response, responseStream);
+            _logger.Debug("📤 Serialized JSON Output: {@RawResponse}", response);
         }
-        else
-        {
-            JsonSerializer.Serialize(responseStream, response, _options);
-        }
+
+        JsonSerializer.Serialize(responseStream, response, _options);
     }
-    private void SerializeWithLogging<T>(T response, Stream responseStream)
-    {
-        var json = JsonSerializer.Serialize(response, _options);
-
-        _logger.Debug("📤 Serialized JSON Output: {Json}", json);
-
-        using var writer = new StreamWriter(responseStream, leaveOpen: true);
-        writer.Write(json);
-        writer.Flush();
-        responseStream.Position = 0;
-    }
-
 }
